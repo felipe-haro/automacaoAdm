f678c04b3920ad94856bed90945331a42f76f2cc
FELIPE HARO PV <felipecaue@psicologiaviva.com.br>
2023-02-13 18:11:57 -0300

    Merge das alterações do curso basico

--- cypress/e2e/admPsicologosPviva.cy.js
+++ cypress/e2e/admPsicologosPviva.cy.js
@@ -1,33 +1,33 @@
 /// <reference types="cypress" />
 
 describe('Suite Módulo Psicólogos', () => {
-    
+
   beforeEach(() => {
     cy.visit('/', {
-    onBeforeLoad(win) {
-      cy.stub(win, "open").as("novaAba")
+      onBeforeLoad(win) {
+        cy.stub(win, "open").as("novaAba")
 
-    }
-  })
-    
+      }
+    })
+
   })
 
 
   context('Valida Psicólogos Ativos na Plataforma pelo ADM Consultório', () => {
-    
-      const usuario = 'felipecaue@psicologiaviva.com.br';
-      const senha = '12345678@';
-        
-      it('realizar busca de um profissional e Validando email, se ele esta ativo e se a cor do ativo esta correta', () => {
+
+    const usuario = 'felipecaue@psicologiaviva.com.br';
+    const senha = '12345678@';
+
+    it('realizar busca de um profissional e Validando email, se ele esta ativo e se a cor do ativo esta correta', () => {
 
       cy.realizarLogin(usuario, senha)
-      
+
       cy.contains("Profissionais")
         .click()
 
       cy.get('#nome_idPesquisa')
         .type('Elise Tes')
-      
+
       cy.get('#btn_pesquisar')
         .click()
 
@@ -36,46 +36,38 @@ describe('Suite Módulo Psicólogos', () => {
         .should('have.css', 'background-color', 'rgb(10, 138, 15)')
         .get('td:contains(seuemail@cicloceap.com.br)')
         .should('have.text', 'seuemail@cicloceap.com.br')
-        })
+    })
   })
-  
+
   context('Realiza Acesso Master com Psicólogo ativo encontrado no ADM Consultório', () => {
-    
+
     const usuario = 'felipecaue@psicologiaviva.com.br';
     const senha = '12345678@';
-    
+
     it('realizar busca de profissional e fazer um acesso master no perfil', () => {
 
       cy.realizarLogin(usuario, senha)
-      
+
       cy.contains("Profissionais")
         .click()
 
       cy.get('#nome_idPesquisa')
         .type('Elise Tes')
-      
+
       cy.get('#btn_pesquisar')
         .click()
 
       cy.get("#btn_acoes").click()
         .get('#bntAcessoMasterProf').as("acessoMasterNewTab")
-        
+
       cy.get("@acessoMasterNewTab").click()
 
       cy.origin('@novaAba', () => {
-        
+
       })
-      
+
 
     })
   })
 
 })
-
-
-
-
-
-
-
-
